e07d141f1be1efdcc33b3a2e5f9fad56abc89b05
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-05-19 07:52:51 +0700

    move

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+//
+//Route::get("/ppe",function (){
+//    return \App\Models\User::all();;
+//});
+//Route::post("/register", [\ppe\dvtinh\Http\Controllers\AuthController::class, 'register']);
+
+Route::group(['prefix' => 'ppe-core'], function() {
+    Route::post("/register", [\ppe\dvtinh\Http\Controllers\AuthController::class, 'register']);
+});
