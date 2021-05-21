c9459dec7e7c2eefddc76b9b849bcb0b3171495c
dvTinh99 <tinh5969@gmail.com>
2021-05-21 10:16:24 +0700

    add logout user function

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,11 @@ Route::get("/ppe", function (){
 Route::group(['prefix' => '/ppe-core/auth'], function() {
     Route::post("/register", [AuthController::class, 'register']);
     Route::post("/login", [AuthController::class, 'login']);
-    Route::get("/logout", [AuthController::class, 'logout']);
+    Route::get('/logout', [AuthController::class, 'logout']);
     Route::get('/generate-url',[AuthController::class,'generateUrl']);
     Route::get('/handle',[AuthController::class,'authHandle']);
 });
-Auth::routes(['verify' => true]);
-Route::get('profile', function () {
-    // Only verified users may enter...
-})->middleware('verified');
+//Auth::routes(['verify' => true]);
 
 
 
