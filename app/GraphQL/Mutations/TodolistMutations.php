1b90a1b21bc9065c5e4838ac74e72e664d81da19
dvTinh99 <tinh5969@gmail.com>
2021-07-06 16:59:32 +0700

    upsert_todolist

--- app/GraphQL/Mutations/TodolistMutations.php
+++ app/GraphQL/Mutations/TodolistMutations.php
@@ -2,12 +2,16 @@
 
 namespace App\GraphQL\Mutations;
 
+use App\Models\GeneralInfo;
+use App\Models\Task;
 use App\Models\Todolist;
 use App\Repositories\GeneralInfoRepository;
 use App\Repositories\GoalRepository;
 use App\Repositories\TodolistRepository;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use App\Repositories\TaskRepository;
+
 
 class TodolistMutations
 {
@@ -15,15 +19,18 @@ class TodolistMutations
     private $todolist_repository;
     private $goal_repository;
     private $generalinfo_repository;
+    private $task_repository;
 
     public function __construct(
         GeneralInfoRepository $generalinfo_repository,
         TodolistRepository $TodolistRepository,
-        GoalRepository $GoalRepository
+        GoalRepository $GoalRepository,
+        TaskRepository $task_repository
     ) {
         $this->generalinfo_repository = $generalinfo_repository;
         $this->todolist_repository = $TodolistRepository;
         $this->goal_repository = $GoalRepository;
+        $this->task_repository = $task_repository ;
     }
 
     public function createTodolist($_, array $args): Todolist
@@ -47,11 +54,14 @@ class TodolistMutations
         $args['goal_id'] = @$goal->id;
         $args['checked_at'] = $args['checked_at'];
         $args['checked_at'] =  date('Y-m-d 00:00:00', strtotime($args['checked_at']));
-
+        $generalTask = GeneralInfo::where("task_id",$args['task_id'])->first();
         $todolist = Todolist::updateOrCreate(
             ['task_id' => $args['task_id'], 'checked_at' => $args['checked_at']],
             $args
         );
+        if (!isset($args["general_info"]["color"])){
+            $args["general_info"]["color"] = $generalTask->color;
+        }
         $generalInfo = $this->generalinfo_repository
             ->setType('todolist')
             ->upsert(array_merge($todolist->toArray(), $args))
@@ -60,7 +70,7 @@ class TodolistMutations
         return $todolist;
     }
 
-    public function upsertTodolist($_, array $args): Todolist
+    public function upsertTodolist($_, array $args)
     {
         return $this->_updateOrCreate($args);
     }
