679bc3cb5fd278b48a4aabcd2d23afdfd6dddaf7
HoaiNam-1632 <nhnam1632@gmail.com>
2021-08-11 09:45:46 +0700

    update model japanneseGoal

---
+++ app/Models/JapaneseGoal.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class JapaneseGoal extends Model
+{
+    use \Staudenmeir\EloquentJsonRelations\HasJsonRelationships;
+    use HasFactory;
+    use SoftDeletes;
+    protected $connection = "mysql";
+    protected $table = "japanese_goals";
+
+    protected $fillable = [
+        'id',
+        'goal_id',
+        'user_id',
+        'type',
+        'more',
+        'score',
+        'created_at',
+        'updated_at'
+    ];
+    protected $casts = [
+        'more' => 'json',
+    ];
+    public function goal(){
+        return $this->belongsTo(goal::class);
+    }
+
+}
