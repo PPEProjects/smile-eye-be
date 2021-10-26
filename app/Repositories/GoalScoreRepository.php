4ca834bcb18a3e52981f97cbb7df233dedc76b33
HoaiNam-1632 <nhnam1632@gmail.com>
2021-10-26 10:41:55 +0700

    update goal_score

--- app/Repositories/GoalScoreRepository.php
+++ app/Repositories/GoalScoreRepository.php
@@ -10,11 +10,14 @@ class GoalScoreRepository{
     public function upsertGoalScore($args)
     {
         $args['user_id'] = Auth::id();
-        $goalScore = GoalScore::updateOrCreate(
-            ['user_id' => $args['user_id'], 'goal_id' => $args['goal_id']],
-            $args
-        );
-        return $goalScore;
+        if(isset($args['goal_id'])){
+            $goalScore = GoalScore::updateOrCreate(
+                ['user_id' => $args['user_id'], 'goal_id' => $args['goal_id']],
+                $args
+            );
+            return $goalScore;
+        }
+        return ;
     }
 
     public function updateGoalScore($args)
