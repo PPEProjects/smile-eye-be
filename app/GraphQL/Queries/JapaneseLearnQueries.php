89007504617821157121af38927dd1ab70892f13
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-11-03 22:49:43 +0700

    fix invite

--- app/GraphQL/Queries/JapaneseLearnQueries.php
+++ app/GraphQL/Queries/JapaneseLearnQueries.php
@@ -2,23 +2,36 @@
 
 namespace App\GraphQL\Queries;
 
-use App\Models\Goal;
 use App\Models\JapaneseLearn;
 use App\Repositories\JapaneseLearnRepository;
-use Illuminate\Support\Facades\Auth;
 
 class JapaneseLearnQueries
 {
-  private $japanese_learn_repository;
-  public function __construct(JapaneseLearnRepository $japanese_learn_repository)
-  {
-      $this->japanese_learn_repository = $japanese_learn_repository;
-  }
- public function myJapaneseLearn(){
+    private $japanese_learn_repository;
+
+    public function __construct(JapaneseLearnRepository $japanese_learn_repository)
+    {
+        $this->japanese_learn_repository = $japanese_learn_repository;
+    }
+
+    public function myJapaneseLearn()
+    {
         return $this->japanese_learn_repository->myJapaneseLearn();
- }
- public function detailJapaneseLearn($_,array $args){
-  return $this->japanese_learn_repository->detailJapaneseLearn($args);
- }
- 
+    }
+
+    public function detailJapaneseLearn($_, array $args)
+    {
+        return $this->japanese_learn_repository->detailJapaneseLearn($args);
+    }
+
+    public function listUserIDJapaneseLearn($_, array $args)
+    {
+        $userIds = JapaneseLearn::where('goal_id', $args['goal_id'])
+            ->get()
+            ->pluck('user_id')
+            ->unique()
+            ->toArray();
+        return ['user_ids' => $userIds];
+    }
+
 }
