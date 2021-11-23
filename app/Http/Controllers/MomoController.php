21ead4372a4580363f7d82e2656dcd89c274159c
tiennv.ppe@gmail.com <tiennv.ppe@gmail.com>
2021-11-23 16:40:52 +0700

    momo

--- app/Http/Controllers/MomoController.php
+++ app/Http/Controllers/MomoController.php
@@ -51,10 +51,7 @@ class MomoController extends Controller
         $request = new \GuzzleHttp\Psr7\Request('POST', $endpoint);
         $response = $client->send($request, [
             'body'    => json_encode($data),
-            'headers' => [
-                'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
-                'accept'     => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-            ]
+            'headers' => ['User-Agent' => 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36']
         ]);
         $result = $response->getBody()->getContents();
         $result = json_decode($result, true);
