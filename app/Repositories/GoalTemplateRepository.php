bd258e5aeb6d721cf0b053e7d5bfbbb126896913
HoaiNam-1632 <nhnam1632@gmail.com>
2021-11-29 10:10:56 +0700

    update goal_template

--- app/Repositories/GoalTemplateRepository.php
+++ app/Repositories/GoalTemplateRepository.php
@@ -10,15 +10,20 @@ use Illuminate\Support\Facades\DB;
 class GoalTemplateRepository{
 
     private $goalMember_repository;
+    private $generalInfo_repository;
     public function __construct(
-        GoalMemberRepository $goalMember_repository
+        GoalMemberRepository $goalMember_repository,
+        GeneralInfoRepository $generalInfo_repository
     ) {
         $this->goalMember_repository = $goalMember_repository;
+        $this->generalInfo_repository = $generalInfo_repository;
     }
     public function createGoalTemplate($args)
     {
         $args['user_id'] = Auth::id();
-            return GoalTemplate::create($args);
+            return GoalTemplate::updateOrCreate([
+                        'goal_id' => $args['goal_id']
+                        ],$args);
     }
 
     public function updateGoalTemplate($args)
@@ -62,12 +67,16 @@ class GoalTemplateRepository{
         }     
             
         $goalIds = $goalTemplate->pluck('goal_id');
-        $goals = Goal::whereIn('id', @$goalIds ?? [])->get();
+        $goals = Goal::whereIn('id', @$goalIds ?? [])->get()->keyBy('id');
+        $goals = $this->generalInfo_repository
+            ->setType('goal')
+            ->get($goals);
         $getId = $goals->pluck('id');
         $goalTemplate = $goalTemplate->whereIn('goal_id', @$getId ?? [])
                                     ->sortByDESC('id'); 
-        $goalTemplate = $goalTemplate->map(function($template) {
+        $goalTemplate = $goalTemplate->map(function($template) use($goals) {
            $goalMember = $this->goalMember_repository->CountNumberMemberGoal($template->goal_id);
+           $template->goal = @$goals[$template->goal_id];
            $template->number_member = $goalMember->number_member; 
             return $template;
         });
