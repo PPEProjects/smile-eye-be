3e9eaa56088d8c9306c640735d06f6d189447338
HoaiNam-1632 <nhnam1632@gmail.com>
2021-12-24 16:24:52 +0700

    update detial_japanese_goal

--- app/Repositories/JapaneseGoalRepository.php
+++ app/Repositories/JapaneseGoalRepository.php
@@ -253,9 +253,10 @@ class JapaneseGoalRepository
             $keyNext = 0;
             $keyPrev = 0;
             if (isset($goalRoot->id)) {
-                $childrenIds = $this->findBlock([$goalRoot->id]);
+                $listGoals = Goal::where('root_id', $goalRoot->id)->orderByRaw('-`index` DESC')->get();
+                $childrenIds = $this->findBlock($listGoals, [$goalRoot->id]);
                 $findIds = array_search($detailJPGoal->goal_id, $childrenIds, true);
-                $trials = $this->findBlock(@$goalRoot->trial_block ?? []);
+                $trials = $this->findBlock($listGoals, @$goalRoot->trial_block ?? []);
                 $trialIds = array_intersect($childrenIds, @$trials ?? []);
                 $checkTrial = in_array($detailJPGoal->goal_id, $trialIds);
 
@@ -354,15 +355,16 @@ class JapaneseGoalRepository
         }
         return $detailJPGoal;
     }
-    public function findBlock($ids, $children = [])
+    public function findBlock($listGoals, $ids, $children = [])
     {
         $getchildren = $children;
+        $goals = $listGoals;
         foreach($ids as $value)
         {
-            $find = Goal::where('parent_id', $value)->orderByRaw('-`index` DESC')->get();
+            $find = $goals->where('parent_id', $value);
             if($find->toArray() != []){
                 $idParent = $find->pluck('id')->toArray();
-                $getchildren =  self::findBlock($idParent, $getchildren);
+                $getchildren =  self::findBlock($listGoals, $idParent, $getchildren);
             }
             else  $getchildren[] = (string)$value;
         }
