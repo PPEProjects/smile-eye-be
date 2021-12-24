2aebc27693bbfd83196f7869b33acdcd827e1343
HoaiNam-1632 <nhnam1632@gmail.com>
2021-12-24 10:20:01 +0700

    update upsert_goal_member

--- app/GraphQL/Mutations/GoalMemberMutations.php
+++ app/GraphQL/Mutations/GoalMemberMutations.php
@@ -4,6 +4,8 @@ namespace App\GraphQL\Mutations;
 
 use App\Models\Goal;
 use App\Models\GoalMember;
+use App\Models\GoalTemplate;
+use App\Models\Payment;
 use App\Repositories\GoalMemberRepository;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,6 +27,7 @@ class GoalMemberMutations
     public function upsertGoalMember($_, array $args)
     {
         $args['user_id'] = Auth::id();
+        $status = ['accept', 'paused', 'confirmed', "paidConfirmed", "done"];
         if(!isset($args['teacher_id']) && isset($args['goal_id'])){
             $goal = Goal::find($args['goal_id']);
             $args['teacher_id'] = @$goal->user_id ?? $args['user_id'];
@@ -39,6 +42,20 @@ class GoalMemberMutations
                     ],
                     $arr
                 );
+                $payment= Payment::where('goal_id', $goal_id)
+                                    ->where("add_user_id", $arr['add_user_id'])
+                                    ->first();
+                $template = GoalTemplate::where('goal_id', $goal_id)
+                                        ->whereIn('status', $status)
+                                        ->first();
+                if(!isset($payment) && isset($template)){
+                    $data = [
+                        'goal_id' => $goal_id,
+                        'add_user_id' => $arr['add_user_id'],
+                        'status' => 'trial'
+                    ];
+                   $createPayment = Payment::create($data);
+                }
             }
             return GoalMember::whereIn('goal_id', $args['goal_ids'])
                 ->where('add_user_id', $args['add_user_id'])
